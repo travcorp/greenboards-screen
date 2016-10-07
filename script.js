99e665e06564a4b8ab74086b940bfc54be2fbd19
Sacha.Zvetelman <sacha.zvetelman@travcorpuk.com>
2016-10-07 14:50:13 +0100

    Some refactorings and added some comments to work on the tops calculation.

--- script.js
+++ script.js
@@ -16,27 +16,36 @@
 			url: 'https://hsgfjonc3m.execute-api.eu-west-1.amazonaws.com/prod/boardscores'
 		}).then(function (response) {
 			if (response.data){
+				const minTop = 45;
+				const maxTop = 5;
 				
-				var cyberForceScore, centerForwardsScore, dramaSquatsScore = 0;
-				
-				for (var i = 0; i < response.data.length; i++){
-					if (response.data[i].team == "CenterForwards"){
-						centerForwardsScore = response.data[i].score
-					}
-					if (response.data[i].team == "CyberForce"){
-						cyberForceScore = response.data[i].score
-					}
-					if (response.data[i].team == "DramaSquats"){
-						dramaSquatsScore = response.data[i].score
-					}
+				var results = response.data;
+
+				//var cfwTop = 
+				//
+				var tops = {
+					CenterForwards : 0,
+					CyberForce: 0,
+					DramaSquats: 0
 				}
 
-				$scope.scores = {
-					centerForwards: centerForwardsScore,     
-					cyberForce: cyberForceScore,
-					dramaSquats: dramaSquatsScore
-				};
+				// tops[resuls.max(m => m.score).name] = maxTop
+				// 
+
+				$scope.teams = [
+					{ name: 'CenterForwards', label: 'CFW', score: findTeamByName('CenterForwards').score, top: maxTop, color: '#48a771' },    
+					{ name: 'CyberForce', label: 'CYF', score: findTeamByName('CyberForce').score, top: 25, color: '#4881a7' },
+					{ name: 'DramaSquats', label: 'DSQ', score: findTeamByName('DramaSquats').score, top: minTop, color: '#a75248' }
+				];
+			}
+
+			function findTeamByName(name) {
+				return results.find((team) => { return team.team === name});
 			}	
+
+			function getMaxScore() {
+
+			}
 			
 		}, function (error) {
 			console.log('Error:' + error);
